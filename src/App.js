9788d089070dfabb7576e8d7ff3a7a40fc0a4e66
Hammad39 <52834653+Hammad39@users.noreply.github.com>
2021-10-20 23:12:52 +0500

    UPDATED MISSING DEPENDENCY

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-daterangepicker/daterangepicker.css';
 import './App.css';
 import Login from './pages/Login';
 import Register from './pages/Register'
